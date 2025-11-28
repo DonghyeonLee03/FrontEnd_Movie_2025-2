3b0dbeb49b289178e5a35dc0bd95ed99d5e067d3
DonghyeonLee03 <leedh0921@naver.com>
2025-11-28 18:23:40 +0900

    7주차 기능 구현완료

--- src/index.js
+++ src/index.js
@@ -1,47 +1,97 @@
-import { listArea, searchInput, listInfo, homeLogo, searchUI, moreBtn, closeBtn } from "./ui/elements.js";
-import { makeList } from "./movie/makeList.js";
-import { removeItem } from "./utils/removeItem.js";
-import { openModal, closeModal } from "./movie/modal.js";
-import { getGenre } from "./apis/getGenre.js";
+import {
+  listArea,
+  searchInput,
+  listInfo,
+  homeLogo,
+  searchUI,
+  moreBtn,
+  closeBtn,
+  infoUI,
+  movieTitle,
+} from './ui/elements.js';
+import { makeList } from './movie/makeList.js';
+import { removeItem } from './utils/removeItem.js';
+import { openModal, closeModal } from './movie/modal.js';
+import { getGenre } from './apis/getGenre.js';
+import { myScore, setStar } from './utils/myScore.js';
 
-let currentMode = "popular";
+let currentMode = 'popular';
 
-moreBtn.addEventListener('click', function(){
-  const currentKeyword = localStorage.getItem("keyword") || null;
+moreBtn.addEventListener('click', function () {
+  const currentKeyword = localStorage.getItem('keyword') || null;
   makeList(currentMode, currentKeyword);
 });
-homeLogo.addEventListener('click', function(){
-  listInfo.textContent = "지금 인기있는 영화";
+homeLogo.addEventListener('click', function () {
+  listInfo.textContent = '지금 인기있는 영화';
   listArea.innerHTML = '';
   searchInput.value = '';
-  currentMode = "popular";
+  currentMode = 'popular';
   makeList(currentMode);
 });
-searchUI.addEventListener('submit', function(e){
+searchUI.addEventListener('submit', function (e) {
   e.preventDefault();
   listInfo.textContent = `"${searchInput.value}" 검색 결과`;
   listArea.innerHTML = '';
-  currentMode = "search";
-  let currentKeyword = localStorage.getItem("keyword") || null;
-  if(currentKeyword !== searchInput.value){
-    localStorage.setItem("keyword", searchInput.value);
+  currentMode = 'search';
+  let currentKeyword = localStorage.getItem('keyword') || null;
+  if (currentKeyword !== searchInput.value) {
+    localStorage.setItem('keyword', searchInput.value);
     removeItem(currentKeyword);
-    currentKeyword = localStorage.getItem("keyword");
+    currentKeyword = localStorage.getItem('keyword');
   }
   makeList(currentMode, currentKeyword);
   searchInput.value = '';
 });
 
-listArea.addEventListener('click', (e)=>{
-  const movie = e.target.closest("#movie");
-  if(!movie)  return;
+listArea.addEventListener('click', (e) => {
+  const movie = e.target.closest('#movie');
+  if (!movie) return;
   getGenre();
   openModal(movie);
-})
+  const score = myScore(movieTitle.textContent);
+  setStar(score);
+});
 
 closeBtn.addEventListener('click', closeModal);
+window.addEventListener('keydown', (e) => {
+  if (e.key === 'Escape') {
+    closeModal();
+  }
+});
 
-(function init(){
-  currentMode = "popular";
+infoUI.addEventListener('mouseover', (e) => {
+  const scoreBtn = e.target.closest('.scoreBtn');
+  if (scoreBtn) {
+    const score = myScore(movieTitle.textContent);
+    setStar(score);
+    let currentNode = scoreBtn;
+    while (currentNode !== null) {
+      const img = currentNode.querySelector('.myScore');
+      if (img) {
+        img.src = '../src/assets/star.svg';
+      }
+      currentNode = currentNode.previousSibling;
+    }
+  }
+});
+
+infoUI.addEventListener('mouseout', () => {
+  const score = myScore(movieTitle.textContent);
+  setStar(score);
+});
+
+infoUI.addEventListener('click', (e) => {
+  const btnTarget = e.target.closest('.scoreBtn');
+  const scoreBtn = infoUI.querySelectorAll('.scoreBtn');
+  const btnArray = Array.from(scoreBtn);
+  const index = btnArray.indexOf(btnTarget);
+
+  myScore(movieTitle.textContent, index);
+  setStar(index);
+});
+
+(function init() {
+  currentMode = 'popular';
   makeList(currentMode);
+  getGenre();
 })();
--- src/movie/modal.js
+++ src/movie/modal.js
@@ -1,25 +1,25 @@
-import { modal, movieTitle } from "../ui/elements.js";
-import { modalResult } from "../ui/interactive.js";
+import { modal, infoUI } from '../ui/elements.js';
+import { modalResult } from '../ui/interactive.js';
 
-export function openModal(movie){
-  try{
-    modal.style.display = "flex";
-    const title = movie.querySelector("#title").textContent;
-    for (let i=0;i<localStorage.length;i++){
+export function openModal(movie) {
+  try {
+    modal.style.display = 'flex';
+    const title = movie.querySelector('#title').textContent;
+    for (let i = 0; i < localStorage.length; i++) {
       let key = localStorage.key(i);
 
       const cacheData = localStorage.getItem(key);
-      if(cacheData){
+      if (cacheData) {
         const res = JSON.parse(cacheData);
         modalResult(res.results, title);
       }
     }
-  } catch(error) {
+  } catch (error) {
     return;
   }
 }
 
-export function closeModal(){
-  modal.style.display = "none";
-  document.querySelector("#movieInfo").innerHTML = '';
+export function closeModal() {
+  modal.style.display = 'none';
+  infoUI.innerHTML = '';
 }
