a6f6635383bc8a47ace621ea5876415232cd5cfb
DonghyeonLee03 <leedh0921@naver.com>
2025-12-21 20:41:03 +0900

    8주차 마이그레이션

---
+++ src/api/getGenre.js
@@ -0,0 +1,13 @@
+import { BASE_URL } from '../constants/index.js';
+import { options } from './apiOptions.js';
+
+export async function getGenre() {
+  try {
+    const response = await fetch(`${BASE_URL}genre/movie/list?language=ko`, options);
+    const res = await response.json();
+    return res;
+  } catch (error) {
+    console.error('Error:', error);
+    return null;
+  }
+}
